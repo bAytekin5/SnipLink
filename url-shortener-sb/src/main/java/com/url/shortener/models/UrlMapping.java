5eb8b5d0eae181df50c8c2f7698e84af3c4b357a
Berkay Aytekin <berkay.0567@gmail.com>
2025-07-20 01:24:57 +0300

    first

---
+++ url-shortener-sb/src/main/java/com/url/shortener/models/UrlMapping.java
@@ -0,0 +1,26 @@
+package com.url.shortener.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Data
+public class UrlMapping {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String original_url;
+    private String short_url;
+    private int click_count = 0;
+    private LocalDateTime created_at;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "urlMapping")
+    private List<ClickEvent> clickEvents;
+}
